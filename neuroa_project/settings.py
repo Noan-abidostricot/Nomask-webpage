10601a05ce3daf85ceb31545d7f0c2d287bb7d12
Lylais <hollyleduc@protonmail.com>
2025-07-04 21:53:53 +0200

    update

--- neuroa_project/settings.py
+++ neuroa_project/settings.py
@@ -21,12 +21,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = [
-    "nomask-webpage.onrender.com",
-    "localhost",
-    "127.0.0.1",
-    # ajoute d’autres domaines si besoin
-]
+ALLOWED_HOSTS = ["nomask-webpage.onrender.com", "localhost", "127.0.0.1"]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
